9fee2b725208171ba425bb14241fe5ec87a4cb20
juangro <ing.juangro@gmail.com>
2018-04-06 12:48:01 -0500

    Reorganize project

---
+++ js/environment/floor.js
@@ -0,0 +1,22 @@
+/**
+ * Function to build the floor with a texture given the size for the plane
+ */
+function buildFloor(environment_size, floor_texture_path) {
+    var geometry = new THREE.BoxGeometry(environment_size, environment_size, 1); // plane of 1000px in x and y
+    var material = new THREE.MeshLambertMaterial({
+      map: new THREE.TextureLoader().load(floor_texture_path, function ( texture ) {
+        texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+        texture.offset.set( 0, 0 );
+        texture.repeat.set( 10, 10 ); // repeat image ten times to cover the 1000px
+      })
+    });
+    var mesh = new THREE.Mesh(geometry, material);
+    mesh.position.x = 0;
+    mesh.position.y = 0;
+    mesh.position.z = 0;
+    scene.add(mesh);
+}
+
+/*------------- Configuring the environment ----------------------- */
+var floor_texture_path = '../img/floor.png';
+buildFloor(environment_size, floor_texture_path);
--- js/environment/walls.js
+++ js/environment/walls.js
@@ -18,25 +18,6 @@ function buildWall(x_position, y_position, z_position, width_wall, height_wall, 
 }
 
 /**
- * Function to build the floor with a texture given the size for the plane
- */
-function buildFloor(environment_size, floor_texture_path) {
-    var geometry = new THREE.BoxGeometry(environment_size, environment_size, 1); // plane of 1000px in x and y
-    var material = new THREE.MeshLambertMaterial({
-      map: new THREE.TextureLoader().load(floor_texture_path, function ( texture ) {
-        texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
-        texture.offset.set( 0, 0 );
-        texture.repeat.set( 10, 10 ); // repeat image ten times to cover the 1000px
-      })
-    });
-    var mesh = new THREE.Mesh(geometry, material);
-    mesh.position.x = 0;
-    mesh.position.y = 0;
-    mesh.position.z = 0;
-    scene.add(mesh);
-}
-
-/**
  * Function to build all the limits given the size of the plane
  */
 function buildLimits(environment_size, wall_texture_path) {
@@ -76,8 +57,4 @@ function buildLimits(environment_size, wall_texture_path) {
 
 /*------------- Configuring the environment ----------------------- */
 var wall_texture_path = '../img/wall.png';
-var floor_texture_path = '../img/floor.png';
-
-// Build environment
 buildLimits(environment_size, wall_texture_path);
-buildFloor(environment_size, floor_texture_path);
