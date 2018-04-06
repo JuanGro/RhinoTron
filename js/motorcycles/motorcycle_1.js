7eb8ed6987acc2874d1bf9b8865a3f78131bb743
juangro <ing.juangro@gmail.com>
2018-04-06 13:14:15 -0500

    Change moto paths

--- js/motorcycles/motorcycle_1.js
+++ js/motorcycles/motorcycle_1.js
@@ -1,3 +1,3 @@
-var moto_json_path = '../models/classic-1982-tron-light-cycle-red.json';
+var motorcycle_1_json_path = '../models/classic-1982-tron-light-cycle-red.json';
 
-buildMoto(moto_json_path, "motorcycle_1", scene, 0, environment_size / 4); // Set in a positive location
+buildMoto(motorcycle_1_json_path, "motorcycle_1", scene, 0, environment_size / 4); // Set in a positive location
--- js/motorcycles/motorcycle_2.js
+++ js/motorcycles/motorcycle_2.js
@@ -1,3 +1,3 @@
-var moto_json_path = '../models/classic-1982-tron-light-cycle-green.json';
+var motorcycle_2_json_path = '../models/classic-1982-tron-light-cycle-green.json';
 
-buildMoto(moto_json_path, "motorcycle_2", scene, -environment_size / 4, 0); // Set in a negative location
+buildMoto(motorcycle_2_json_path, "motorcycle_2", scene, -environment_size / 4, 0); // Set in a negative location
