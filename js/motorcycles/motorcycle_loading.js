0becb23bd494f40f473fc1a6377194aa9c0fda4a
juangro <ing.juangro@gmail.com>
2018-04-20 13:32:09 -0500

    Refactor code

--- js/motorcycles/motorcycle_loading.js
+++ js/motorcycles/motorcycle_loading.js
@@ -4,21 +4,21 @@ function buildMoto(moto_json_path, moto_name, scene, min_position, max_position)
         // resource URL
         moto_json_path,
         // called when resource is loaded
-        function(moto) {
+        function(motorcycle) {
             // Add motorcycle in a random position between min_position and max_position
-            moto.position.x = getRandomArbitrary(min_position, max_position);
-            moto.position.y = getRandomArbitrary(min_position, max_position);
-            moto.position.z = 0;
-            // Size of the moto
-            moto.scale.set(15, 10, 10);
+            motorcycle.position.x = getRandomArbitrary(min_position, max_position);
+            motorcycle.position.y = getRandomArbitrary(min_position, max_position);
+            motorcycle.position.z = 0;
+            // Size of the motorcycle
+            motorcycle.scale.set(15, 10, 10);
             // Rotation to place it
-            moto.rotation.x = Math.PI / 2;
-            moto.rotation.y = 0;
-            moto.rotation.z = 0;
-            // Name for the moto
-            moto.name = moto_name;
-            // Add moto to the environment
-            scene.add(moto);
+            motorcycle.rotation.x = Math.PI / 2;
+            motorcycle.rotation.y = 0;
+            motorcycle.rotation.z = 0;
+            // Name for the motorcycle
+            motorcycle.name = moto_name;
+            // Add motorcycle to the environment
+            scene.add(motorcycle);
         },
         // called when loading is in progresses
         function(xhr) {
@@ -31,28 +31,28 @@ function buildMoto(moto_json_path, moto_name, scene, min_position, max_position)
     );
 }
 
-function randomPosition(moto1,moto2,min_position, max_position,orientation){
-        moto1.position.x = getRandomArbitrary(min_position, max_position);
-        moto1.position.y = getRandomArbitrary(min_position, max_position);
-        moto1.position.z = 0;
-        //moto1.rotation.y= Math.PI;
+function randomPosition(current_motorcycle, opponent_motorcycle, min_position, max_position, orientation) {
+        current_motorcycle.position.x = getRandomArbitrary(min_position, max_position);
+        current_motorcycle.position.y = getRandomArbitrary(min_position, max_position);
+        current_motorcycle.position.z = 0;
+        //current_motorcycle.rotation.y= Math.PI;
 
-        moto2.position.x = -moto1.position.x;
-        moto2.position.y = -moto1.position.y;
-        moto2.position.z = 0;
+        opponent_motorcycle.position.x = -current_motorcycle.position.x;
+        opponent_motorcycle.position.y = -current_motorcycle.position.y;
+        opponent_motorcycle.position.z = 0;
 
         //POSITION MATCH CAMERA CADA VEZ QUE SE PIERDA
-        player_1_camera.position.x = moto1.position.x;
-        player_1_camera.position.y = moto1.position.y;
+        player_1_camera.position.x = current_motorcycle.position.x;
+        player_1_camera.position.y = current_motorcycle.position.y;
 
         if(orientation == 1) {
-            player_1_camera.position.y = moto1.position.y - camera_remoteness;
+            player_1_camera.position.y = current_motorcycle.position.y - camera_remoteness;
         } else if(orientation == 2) {
-            player_1_camera.position.x = moto1.position.x - camera_remoteness;
+            player_1_camera.position.x = current_motorcycle.position.x - camera_remoteness;
         } else if(orientation == 3) {
-            player_1_camera.position.y = moto1.position.y + camera_remoteness;
+            player_1_camera.position.y = current_motorcycle.position.y + camera_remoteness;
         } else if(orientation == 4) {
-            player_1_camera.position.x = moto1.position.x + camera_remoteness;
+            player_1_camera.position.x = current_motorcycle.position.x + camera_remoteness;
         }
         
         player_1_camera.position.z = camera_position_in_z;
