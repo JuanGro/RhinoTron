45a0dd3da1f8b8383997276d56c637543cfa49ce
papa <papa>
2018-05-01 00:21:20 -0500

    claudio termino la chingadera

---
+++ js/environment/blinker.js
@@ -0,0 +1,28 @@
+function show(){
+          if (document.getElementById && player_1_lifes==0 ){
+              document.getElementById("player_1_scoreboard").style.visibility = "visible";
+          }
+
+          if(document.getElementById && player_2_lifes==0){
+            document.getElementById("player_2_scoreboard").style.visibility = "visible";
+          }
+          
+}
+
+
+function hide(){
+          if (document.getElementById && player_1_lifes==0 ){
+              document.getElementById("player_1_scoreboard").style.visibility = "hidden";
+          }
+
+          if (document.getElementById && player_2_lifes==0 ){
+              document.getElementById("player_2_scoreboard").style.visibility = "hidden";
+          }
+          
+}
+
+
+for(var i=900; i < 1000000; i=i+900){
+          setTimeout("hide()",i);
+          setTimeout("show()",i+450);
+}
