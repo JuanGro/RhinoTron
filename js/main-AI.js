8624974d128c0241fd9af2a766793581311f1d1f
juangro <ing.juangro@gmail.com>
2018-05-02 15:41:51 -0500

    Add independence to the moto

--- js/main-AI.js
+++ js/main-AI.js
@@ -80,7 +80,6 @@ window.onload = function() {
     motorcycle_2 = scene.getObjectByName("motorcycle_2");
     randomPosition(motorcycle_1, motorcycle_2, 0, environment_size / 4, player_1_orientation);
     initMotorcycle1(motorcycle_1, motorcycle_2);
-    initMotorcycle2(motorcycle_2, motorcycle_1);
 }
 
 // Draw scene
@@ -94,6 +93,7 @@ var animate = function() {
     if(motorcycle_1 && motorcycle_2) {
         player_1_tail_flag = continuosMovement(motorcycle_1, motorcycle_2, 'player_1', player_1_orientation, player_1_camera, player_1_color, player_1_tail_flag);
         player_2_tail_flag = continuosMovement(motorcycle_2, motorcycle_1, 'player_2', player_2_orientation, player_2_camera, player_2_color, player_2_tail_flag);
+        moveMotorcycle2(motorcycle_2);
     }
     requestAnimationFrame(animate);
     render();
