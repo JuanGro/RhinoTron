34b9ab0af95112116be5c71351252c775bd72f6e
juangro <ing.juangro@gmail.com>
2018-05-21 16:16:31 -0500

    Fix audio

--- js/songs/songs.js
+++ js/songs/songs.js
@@ -1,37 +1,24 @@
-var song = document.getElementById("song");
-song.volume = 0.13; 
+var song, sound;
 
-var sound;
-
-function playSelectSound(divId) {
-    sound = document.getElementById(divId.id);
-    sound.volume = 1;
-    sound.play();
+function play(divId, typeSound) {
+    if (typeSound == 'song') {
+        song = document.getElementById(divId.id);
+        song.volume = 1;
+        song.play();
+    } else {
+        sound = document.getElementById(divId.id);
+        sound.volume = 1;
+        sound.play();
+    }
 }
 
-var x = document.getElementById("TronLegacy");  
-
-function playAudio(number) { 
-    if(number == 1) {
-        x = document.getElementById("TronLegacy");
-    } else if(number == 2) {
-        x = document.getElementById("EndOfLine"); 
-    } else if(number == 3) {
-        x = document.getElementById("Derezzed"); 
+function pauseSong() {
+    if (song) {
+        song.pause(); 
+        song.load();
     }
-    x.volume = 0.13;
-    x.play(); 
-} 
-
-function pauseAudio() { 
-    x.pause(); 
-    x.load();
 } 
 
 function valueSong(path) {
-    localStorage.setItem("storageName", path);
-}
-
-function play() {
-    sound.play();
+    localStorage.setItem("music_path", path);
 }
