f0a5d5721306a1abcc3226b477d25c949df3e77e
juangro <ing.juangro@gmail.com>
2018-05-21 16:46:36 -0500

    Fix animate

--- js/animate.js
+++ js/animate.js
@@ -1,7 +1,7 @@
 function animate() {
     if (motorcycle_1 && motorcycle_2) {
-        player_1_tail_flag = continuosMovement(motorcycle_1, motorcycle_2, 1, player_1_orientation, player_1_camera, player_1_color, player_1_tail_flag);
-        player_2_tail_flag = continuosMovement(motorcycle_2, motorcycle_1, 2, player_2_orientation, player_2_camera, player_2_color, player_2_tail_flag);
+        player_1_tail_flag = continuosMovement(motorcycle_1, motorcycle_2, 1, player_1_orientation, player_1_camera, player_1_color_hex, player_1_tail_flag);
+        player_2_tail_flag = continuosMovement(motorcycle_2, motorcycle_1, 2, player_2_orientation, player_2_camera, player_2_color_hex, player_2_tail_flag);
     }
     requestAnimationFrame(animate);
     render();
