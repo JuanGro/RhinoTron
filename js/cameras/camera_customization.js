03ccc7c1990dee61fa9e0c10bc59e3b15fbda8f5
juangro <ing.juangro@gmail.com>
2018-05-21 23:40:06 -0500

    Improve UI

--- js/cameras/camera_customization.js
+++ js/cameras/camera_customization.js
@@ -1,12 +1,12 @@
 function startCamera(renderer, player_camera, id_camera) {
   // Specify the size to show in the window
-  renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
+  renderer.setSize(window.innerWidth / 2.5, window.innerHeight / 2.5);
   container = document.getElementById(id_camera);
   container.appendChild(renderer.domElement);
 
   // detect when window resize
   window.addEventListener('resize', function() {
-    renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
+    renderer.setSize(window.innerWidth / 2.5, window.innerHeight / 2.5);
     player_camera.aspect = window.innerWidth / window.innerHeight;
     player_camera.updateProjectionMatrix();
   });
