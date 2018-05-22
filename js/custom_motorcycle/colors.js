7eb35bd3a146bb6906bc41a3344740270e3cef8b
juangro <ing.juangro@gmail.com>
2018-05-22 01:07:47 -0500

    Change model links

--- js/custom_motorcycle/colors.js
+++ js/custom_motorcycle/colors.js
@@ -1,18 +1,18 @@
 function changeColor(color) {
     if (color == 'red') {
-        path = '../../models/classic-1982-tron-light-cycle-red.json';
+        path = '../models/classic-1982-tron-light-cycle-red.json';
         color_hex = 0xff0000;
         color_rgb = 'rgb(255, 0, 0)';
     } else if (color == 'green') {
-        path = '../../models/classic-1982-tron-light-cycle-green.json';
+        path = '../models/classic-1982-tron-light-cycle-green.json';
         color_hex = 0x7CFC00;
         color_rgb = 'rgb(49, 233, 12)';
     } else if (color == 'blue') {
-        path = '../../models/classic-1982-tron-light-cycle-blue.json';
+        path = '../models/classic-1982-tron-light-cycle-blue.json';
         color_hex = 0x00B2FF;
         color_rgb = 'rgb(22, 178, 236)';
     } else if (color == 'pink') {
-        path = '../../models/classic-1982-tron-light-cycle-pink.json';
+        path = '../models/classic-1982-tron-light-cycle-pink.json';
         color_hex = 0xFF6EC7;
         color_rgb = 'rgb(226, 41, 196)';
     }
